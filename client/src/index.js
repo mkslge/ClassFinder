02c8515ad81fc52419de862ce592736faadb3158
mkslge <marklseeliger@gmail.com>
2026-01-15 14:33:18 -0500

    refractored files

--- client/src/index.js
+++ client/src/index.js
@@ -1,9 +1,11 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import './index.css';
-import Header from './Header'
-import Introduction from './Introduction'
-import CourseList from './CourseList';
+
+import Header from './components/Header'
+import Introduction from './components/Introduction'
+import CourseList from './components/CourseList';
+
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
---
+++ client/src/pages/About.js
@@ -0,0 +1,10 @@
+import React from "react"
+import Home from "../index.js";
+
+function About() {
+    return (
+        <p> About Page Testing </p>
+    )
+}
+
+export default About;
